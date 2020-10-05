639492ba12bf13e0f92e524632e813f0aa346b85
albanaguilar <albanaguilar1@gmail.com>
2020-10-04 20:18:48 -0500

    Added search bar in nav

---
+++ public/js/scripts.js
@@ -0,0 +1,9 @@
+// Buscador
+function openSearch() {
+  document.getElementById("overlay").style.display = "block";
+}
+
+function closeSearch(){
+  document.getElementById("overlay").style.display = "none";
+}
+// Fin de Buscador
